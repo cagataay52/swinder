aded2d52eb505831d407a6261642a3a5c8843818
Çağatay Kılıç <104324394+cagataay52@users.noreply.github.com>
2026-03-05 20:26:32 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🔥 SWINDER VIP - AKILLI ÖNBELLEK MOTORU (V21) 🔥
-const CACHE_NAME = 'swinder-vip-v26';
+const CACHE_NAME = 'swinder-vip-v27';
 
 // Sadece en temel çerçeveyi hafızaya al (Hızlı açılış için)
 const ASSETS_TO_CACHE = [
